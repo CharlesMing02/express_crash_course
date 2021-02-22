0e5a63b6b06af85476768d1e493386ae0d3b5a66
Charles Ming <charlesming@berkeley.edu>
2021-02-21 19:43:03 -0500

    First 30 min of tutorial

---
+++ Members.js
@@ -0,0 +1,16 @@
+const members = [
+	{
+		id: 1,
+		name: 'John Doe',
+		email: 'john@gmail.com',
+		status: 'active'
+	},
+	{
+		id: 2,
+		name: 'Charles Ming',
+		email: 'charlesming2002@gmail.com',
+		status: 'inactive'
+	}
+];
+
+module.exports = members;
