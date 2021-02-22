aab776968d55302fd9a26f358eac9651a36f7d57
Charles Ming <charlesming@berkeley.edu>
2021-02-22 02:26:02 -0500

    Finished

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const path = require('path');
-const moment = require('moment');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
 const members = require('./Members');
 
 const app = express();
 
-const logger = (req, res, next) => {
-	console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
-	next();
-}
-
 // Initialize middleware
-app.use(logger);
+// app.use(logger);
 
-//Gets all members
-app.get('/api/members', (req, res) => {
-	res.json(members);
-});
+//Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-//Sets static folder
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Homepage route
+app.get('/', (req, res) => res.render('index', {
+	title: 'Member App',
+	members
+}));
+
+//Sets static folder (doesn't show here because it is below the homepage route)
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Members api routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
